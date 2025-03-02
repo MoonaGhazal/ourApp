bc4a27389dcd392fc605ca9deab09b2bd9e289d0
ShaimX9 <148153100+ShaimX9@users.noreply.github.com>
2025-03-02 16:11:14 +0200

    last change.

--- lib/detail_movie_screen.dart
+++ lib/detail_movie_screen.dart
@@ -9,63 +9,48 @@ class DetailMovieScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        
         child: Stack(
           alignment: Alignment.center,
           children: [
             Image.asset("assets/Video.png",
-                width: double.infinity, 
-                height: 250, 
-                fit: BoxFit.fill
-              ),
+                width: double.infinity, height: 250, fit: BoxFit.fill),
 
+            Text(
+              "kjiuhuh",
+              style: TextStyle(color: Colors.amber),
+            ),
 
             Column(
-              mainAxisSize:MainAxisSize.min, //عشان تكون على قد المحتويات مش الشاشه كلها
+              mainAxisSize:
+                  MainAxisSize.min, //عشان تكون على قد المحتويات مش الشاشه كلها
               children: [
                 IconButton(
                   onPressed: () {},
-                  icon: Icon(
-                    Icons.play_circle, 
-                    size: 45, 
-                    color: Colors.white
-                  ),
+                  icon: Icon(Icons.play_circle, size: 45, color: Colors.white),
                 ),
-
-
                 Text(
                   "Play Trailer",
                   style: TextStyle(
                       color: Colors.white,
                       fontSize: 16,
-                      fontWeight: FontWeight.w600
-                    ),
+                      fontWeight: FontWeight.w600),
                 ),
               ],
             ),
-
 
             //back
             Positioned(
               top: 40,
               left: 10,
-              child: Icon(
-                Icons.arrow_back, 
-                color: Colors.white
-              ),
+              child: Icon(Icons.arrow_back, color: Colors.white),
             ),
-
 
             //more
             Positioned(
               top: 40,
-              left: 10,
-              child: Icon(
-                Icons.more_horiz, 
-                color: Colors.white
-              ),
+              right: 10,
+              child: Icon(Icons.more_horiz, color: Colors.white),
             ),
-
           ],
         ),
       ),
