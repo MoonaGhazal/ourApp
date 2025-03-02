d07fd8a342c7c479635fd78cf796200d19094718
ShaimX9 <148153100+ShaimX9@users.noreply.github.com>
2025-03-02 22:12:05 +0200

    finished title with info movie.

--- lib/detail_movie_screen.dart
+++ lib/detail_movie_screen.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors_in_immutables
 
 import 'package:flutter/material.dart';
+import 'package:our_app/widget/category_widget.dart';
+import 'package:our_app/widget/movie_info_widget.dart';
 
 class DetailMovieScreen extends StatelessWidget {
   DetailMovieScreen({super.key});
@@ -8,51 +10,101 @@ class DetailMovieScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Image.asset("assets/Video.png",
-                width: double.infinity, height: 250, fit: BoxFit.fill),
+      body: Column(
+        children: [
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Image.asset("assets/Video.png",
+                  width: double.infinity, height: 250, fit: BoxFit.fill),
 
-            Text(
-              "kjiuhuh",
-              style: TextStyle(color: Colors.amber),
-            ),
+              Column(
+                mainAxisSize: MainAxisSize
+                    .min, //عشان تكون على قد المحتويات مش الشاشه كلها
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon:
+                        Icon(Icons.play_circle, size: 45, color: Colors.white),
+                  ),
+                  Text(
+                    "Play Trailer",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ],
+              ),
 
-            Column(
-              mainAxisSize:
-                  MainAxisSize.min, //عشان تكون على قد المحتويات مش الشاشه كلها
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.play_circle, size: 45, color: Colors.white),
+              //back
+              Positioned(
+                top: 40,
+                left: 10,
+                child: Icon(Icons.arrow_back, color: Colors.white),
+              ),
+
+              //more
+              Positioned(
+                top: 40,
+                right: 10,
+                child: Icon(Icons.more_horiz, color: Colors.white),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(color: Colors.white),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Spiderman: No Way Home",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.star,
+                          size: 18,
+                          color: Color(0xffFFC319),
+                        ),
+                        SizedBox(width: 4),
+                        Text(
+                          "9.1/10 IMDb",
+                          style: TextStyle(
+                            color: Color(0xff9C9C9C),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 12),
+                    Wrap(
+                      spacing: 16,
+                      children: [
+                        CategoryWidget(label: "ACTION"),
+                        CategoryWidget(label: "ADVENTURE"),
+                        CategoryWidget(label: "FANTASY"),
+                      ],
+                    ),
+                    SizedBox(height: 12),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        MovieInfoWidget(title: "Length", value: "2h 28min"),
+                        MovieInfoWidget(title: "language", value: "English"),
+                        MovieInfoWidget(title: "Rating", value: "PG-13"),
+                      ],
+                    ),
+                  ],
                 ),
-                Text(
-                  "Play Trailer",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600),
-                ),
-              ],
+              ),
             ),
-
-            //back
-            Positioned(
-              top: 40,
-              left: 10,
-              child: Icon(Icons.arrow_back, color: Colors.white),
-            ),
-
-            //more
-            Positioned(
-              top: 40,
-              right: 10,
-              child: Icon(Icons.more_horiz, color: Colors.white),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/widget/category_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CategoryWidget extends StatelessWidget {
+  CategoryWidget({super.key, required this.label});
+  String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Chip(
+      
+      label: Text(
+        label,
+        style: TextStyle(
+          fontSize: 12,
+          color: Color(0xff88A4E8),
+        ),
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 2),
+      
+      backgroundColor: Color(0xffDBE3FF),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+    );
+  }
+}
